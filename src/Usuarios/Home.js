5c4e07e951a753bda8bd6336edb53a0802c505f3
anavb2309 <anavalenciaburbano@gmail.com>
2025-01-08 23:17:55 -0500

    proyectReact_4

--- src/Usuarios/Home.js
+++ src/Usuarios/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../imagenes/Logo_interivalle.jpg'; 
 
+
 const Home = () => {
     const navigate = useNavigate();
 
